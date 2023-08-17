2ed919fc8475413b8d66f0f81cb0114d6cf60ba8
backchristoffer <cback@redhat.com>
2023-08-17 14:04:49 +0200

    refactor - using pandas and fixing None and "not finding columns" issue in csv

--- ocpbugpull.py
+++ ocpbugpull.py
@@ -1,48 +1,35 @@
-import csv, re, argparse
+import pandas as pd
+import re, argparse
 
 def reg(m):
     return m.group(1)
 
-def csvhandler(csvfile):
-    with open(csvfile, 'r') as csv_file:
-        reader = csv.DictReader(csv_file)
-        linec = 0
-        buglist = []
-        for row in reader:
-            if linec == 0:
-                #print(f'Column names are {", ".join(row)}')
-                linec += 1
-            #print(f'OCPBUGS rows: {row["Resource URL"]}')
-            while('' in row):
-                row.remove('')
-            buglist.append(row['Resource URL'].translate({ord(i): None for i in 'https://issues.redhat.com/browse/'}))
-            linec += 1
-    return buglist
+def readcsv(csvfile):
+    df = pd.read_csv(csvfile, sep=';', usecols=['Resource URL'], na_values=' NaN')
+    result = df.mask(df.eq('None')).dropna()
+    return result
 
-def findbugs(data):
-    #Remove empty results
-    while('' in data):
-        data.remove('')
-    #Check for duplicates
-    if len(data) == len(set(data)):
-        #print("Found duplicates in list")
-        return False
-    else:
-        #print("No duplicates found")
-        True
+def dataframetostring(data):
+    #Convert Resource URL rows to strings
+    record = data.to_string(header=False,index=False,index_names=False).split('\n')
+    vals = [','.join(ele.split()) for ele in record]
+    return vals
+
+def getocpbugs(lst):
+    buglist = []
+    for row in lst:
+        buglist.append(row.translate({ord(i): None for i in 'https://issues.redhat.com/browse/'}))
     #Convert list to string and do clean-up
     bugstring=' '
-    for x in data:
+    for x in buglist:
         bugstring += '{}, '.format(x)
     bugstring = re.sub("(.*)(.{2}$)",reg,bugstring)
-    
     #Add JQL to string
     result = "project = OCPBUGS AND issuekey in ({})".format(bugstring)
     return result
-
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Import csv from Salesforce to create JQL to find OCPBUGS')
     parser.add_argument('-f', help='path to csv')
     args = parser.parse_args()
-    print(findbugs(csvhandler(csvfile=args.f)))
+    print(getocpbugs(dataframetostring(readcsv(csvfile=args.f))))
